9cafdc805698b65454f1c875d053c496b516b933
redface <redface0512@gmail.com>
2017-05-17 01:13:56 +0900

    experiments route applied

--- src/main-web/app/app-common.module.ts
+++ src/main-web/app/app-common.module.ts
@@ -4,19 +4,19 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { ClarityModule } from 'clarity-angular';
-
 import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { ExperimentsModule } from './experiments/experiments.module';
 import { RequestHandler, RequestExceptionHandler } from './shared/handlers';
 import { AppService } from './app.service';
 
 const NG_MODULES:Array<Type<any> | ModuleWithProviders | any[]> =
   <any>[BrowserModule, CommonModule, HttpModule, FormsModule, ReactiveFormsModule];
 
-const THIRDPARTY_MODULES:Array<Type<any> | ModuleWithProviders | any[]> = [ClarityModule.forRoot()];
-const CUSTOMISED_MODULES:Array<Type<any> | ModuleWithProviders | any[]> = <any>[AppRoutingModule, HomeModule];
+const THIRDPARTY_MODULES:Array<Type<any> | ModuleWithProviders | any[]> = [];
+const CUSTOMISED_MODULES:Array<Type<any> | ModuleWithProviders | any[]> =
+  <any>[AppRoutingModule, HomeModule, ExperimentsModule];
 
 const modules:Array<Type<any> | ModuleWithProviders | any[]> = [NG_MODULES, THIRDPARTY_MODULES, CUSTOMISED_MODULES];
 
--- src/main-web/app/app.routing.module.ts
+++ src/main-web/app/app.routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
 const appRoutes:Routes = <Routes>[
   {
     path: '',
     children: [
-      {path: '', component: HomeComponent}
+      {path: '', component: HomeComponent},
+      {path: 'experiments', component: ExperimentsComponent}
     ]
   },
   {path: '**', redirectTo: ''} // exceptional urls redirected to default url
