9ce462316bd39c9dd1aefb624629aeafd68acefc
redface <redface0512@gmail.com>
2017-05-20 15:22:16 +0900

    grid component skeleton added

---
+++ src/main-web/app/experiment/grid/grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rf-grid',
+  templateUrl: './grid.html',
+  styleUrls: ['./grid.scss']
+})
+export class GridComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
