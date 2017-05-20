7cf132af26573e2c44a28ba4183c6a231a61c7c8
redface <redface0512@gmail.com>
2017-05-20 15:22:43 +0900

    experiment-list component added

---
+++ src/main-web/app/experiment/experiment-list/experiment-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rf-experiment-list',
+  templateUrl: './experiment-list.html',
+  styleUrls: ['./experiment-list.scss']
+})
+export class ExperimentListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
