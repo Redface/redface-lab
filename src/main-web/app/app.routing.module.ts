47ffea6ee3c343d252f0e84d07c850e7356384bc
redface <redface0512@gmail.com>
2017-08-01 08:14:44 +0900

    lazy-loading applied

--- src/main-web/app/app.routing.module.ts
+++ src/main-web/app/app.routing.module.ts
@@ -2,17 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { ExperimentComponent } from './experiment/experiment.component';
 
 // let's talk about pathMatch: full and prefix
 const appRoutes:Routes = <Routes>[
   {
     path: '',
-    children: [
-      {path: '', component: HomeComponent},
-      {path: 'experiment', component: ExperimentComponent}
-    ]
+    component: HomeComponent,
   },
+  {path: 'experiment', loadChildren: './experiment/experiment.module#ExperimentModule'},
   {path: '**', redirectTo: ''}
 ];
 
