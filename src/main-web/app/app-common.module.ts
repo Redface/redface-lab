100b91db4f0f73c49549973583c301e690a73a39
redface <redface0512@gmail.com>
2017-08-01 08:34:12 +0900

    common module imports core module

--- src/main-web/app/app-common.module.ts
+++ src/main-web/app/app-common.module.ts
@@ -1,28 +1,25 @@
 import { Type, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule, Title } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { ExperimentModule } from './experiment/experiment.module';
-import { RequestHandler, RequestExceptionHandler } from './shared/handlers';
 import { AppService } from './app.service';
+import { CoreModule } from './core/core.module';
 
 const NG_MODULES:Array<Type<any> | ModuleWithProviders | any[]> =
-  <any>[BrowserModule, CommonModule, HttpClientModule, FormsModule, ReactiveFormsModule];
+  <any>[BrowserModule];
 
 const THIRDPARTY_MODULES:Array<Type<any> | ModuleWithProviders | any[]> = [];
 const CUSTOMISED_MODULES:Array<Type<any> | ModuleWithProviders | any[]> =
-  <any>[HomeModule, ExperimentModule, AppRoutingModule]; // order matters, AppRoutingModules should come last
+  <any>[CoreModule, HomeModule, ExperimentModule, AppRoutingModule]; // order matters, AppRoutingModules should come last
 
 const modules:Array<Type<any> | ModuleWithProviders | any[]> = [NG_MODULES, THIRDPARTY_MODULES, CUSTOMISED_MODULES];
 
 const NG_PROVIDERS:Array<Type<any> | ModuleWithProviders | any[]> = <any>[Title];
 const THIRDPARTY_PROVIDERS:Array<Type<any> | ModuleWithProviders | any[]> = [];
-const CUSTOMISED_PROVIDERS:Array<Type<any> | ModuleWithProviders | any[]> = <any>[RequestHandler, RequestExceptionHandler, AppService];
+const CUSTOMISED_PROVIDERS:Array<Type<any> | ModuleWithProviders | any[]> = <any>[AppService];
 
 export const NG_MODULE = {
   imports: modules,
