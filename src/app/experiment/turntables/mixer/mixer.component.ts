e3d197ab0e2a3c39a5d85b5e985f5fe0219dddac
redface <redface0512@gmail.com>
2018-05-09 00:09:30 +0900

    mixer component template added

---
+++ src/app/experiment/turntables/mixer/mixer.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'rf-mixer',
+  templateUrl: './mixer.component.html',
+  styleUrls: ['./mixer.component.scss']
+})
+export class MixerComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
